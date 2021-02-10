551a3444377691dc0642f75d3e9a881383c44cff
JotLi <18309253707@163.com>
2021-02-10 14:03:23 +0800

    小肥牛管理后台子系统初始化

---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="notfound">
+    <h1>404！NOT FOUND</h1>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+*Vue 的核心配置文件 
+*/
+module.exports = {
+    //webpack开发服务器
+    devServer: {
+        host: '127.0.0.1',
+        port: 8091
+    }
+}
